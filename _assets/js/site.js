84ced76f2f63e2154e6d5646603140b76477a9d9
MartinMagician <mm@swse.com.cn>
2021-06-04 15:49:26 +0800

    Include js

---
+++ _assets/js/site.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b7f0e484b71d814e87aaf98f2030dc94";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
